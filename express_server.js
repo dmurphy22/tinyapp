72d2328e07b5988f9bd2064bd415f089fa5a87bc
Darragh Murphy <dmurphy@servuo.com>
2023-05-15 15:25:41 -0700

    Add bootstrap code to index template

--- express_server.js
+++ express_server.js
@@ -14,12 +14,19 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
