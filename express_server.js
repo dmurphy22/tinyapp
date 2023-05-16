78369245618c07e36dcfc7509c73c5b46f1ac26c
Darragh Murphy <dmurphy@servuo.com>
2023-05-15 17:13:56 -0700

    Add redirect feature of app and updade logic of form return

--- express_server.js
+++ express_server.js
@@ -38,12 +38,36 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const genID = generateRandomString(6);
+  urlDatabase[genID] = req.body["longURL"];
+  console.log(urlDatabase);
+  res.redirect(`/urls/${genID}`); // Respond with 'Ok' (we will replace this)
 });
+
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+const generateRandomString = function(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+
+};
