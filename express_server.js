f745c496b360eb34b2f297519c64b5eb746a2bc8
Darragh Murphy <dmurphy@servuo.com>
2023-05-16 21:04:03 -0700

    Add logout function

--- express_server.js
+++ express_server.js
@@ -77,6 +77,12 @@ app.post("/login", (req, res) => {
  
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+ 
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
